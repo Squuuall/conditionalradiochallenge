19220aea8f9b061a8400e7894d7666c14a5866f0
Josh <josharmstrongphoto@gmail.com>
2018-01-25 10:36:42 -0800

    Initial commit

---
+++ conditionalradiochallenge/WebForm1.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="conditionalradiochallenge.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        Your note taking preferences<br />
+        <br />
+        <asp:RadioButton ID="pencilRadio" runat="server" GroupName="noteGroup" Text="Pencil" />
+        <br />
+        <asp:RadioButton ID="penRadio" runat="server" GroupName="noteGroup" Text="Pen" />
+        <br />
+        <asp:RadioButton ID="phoneRadio" runat="server" GroupName="noteGroup" Text="Phone" />
+        <br />
+        <asp:RadioButton ID="tabletRadio" runat="server" GroupName="noteGroup" Text="Tablet" />
+        <br />
+        <br />
+        <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="ok" />
+        <br />
+        <br />
+        <asp:Image ID="showImage" runat="server" Height="100px" Width="100px" />
+        <br />
+        <br />
+        <asp:Label ID="resultLabel" runat="server"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ conditionalradiochallenge/WebForm1.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace conditionalradiochallenge
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if (pencilRadio.Checked == true)
+                resultLabel.Text = "You selected pencil.";
+            else if (penRadio.Checked == true)
+                resultLabel.Text = "You selected pen.";
+            else if (phoneRadio.Checked == true)
+                resultLabel.Text = "You selected phone.";
+            else if (tabletRadio.Checked == true)
+                resultLabel.Text = "You selected tablet.";
+            else resultLabel.Text = "Please select an option.";
+
+            if (pencilRadio.Checked == true)
+                showImage.ImageUrl = "https://target.scene7.com/is/image/Target/17077875?wid=520&hei=520&fmt=pjpeg";
+            else if (penRadio.Checked == true)
+                showImage.ImageUrl = "https://www.customink.com/mms/images/catalog/styles/103700/catalog_detail_image_large.jpg";
+            else if (phoneRadio.Checked == true)
+                showImage.ImageUrl = "https://i5.walmartimages.com/asr/80bdd9c3-fccb-4ce1-ada3-fe9052d68005_1.eda20373d8d69127055bf1276f19a385.jpeg";
+            else if (tabletRadio.Checked == true)
+                showImage.ImageUrl = "https://gloimg.gbtcdn.com/gb/pdm-product-pic/Electronic/2017/08/19/goods-img/1503107536771803487.jpg";
+        }
+    }
+}
